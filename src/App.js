fa8c5de9812fb5fb8a9cecdec449faffbafc12e0
aahmadhadi <ahmadhadijaelani@gmail.com>
2019-08-09 11:14:45 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Coffee from './components/Coffee';
+import Glass from './components/Glass';
+import Saucer from './components/Saucer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            number : 20,
+        }
+    }
+    handleClick(){
+        this.setState({ number : this.state.number + 2 })
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={()=> this.handleClick()}>
+                    Increase Saucer !
+                </button>
+                <Coffee
+                    waterVolume={1}
+                    bean='Robusta'
+                    type='expresso'
+                />
+                <Glass
+                    color='#F1F1FF'
+                />
+                <Saucer
+                    width={this.state.number}
+                />
+            </div>
+        )
+    }
 }
-
-export default App;
---
+++ src/components/Saucer.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Saucer extends Component {
+    render() {
+        return (
+            <p style={{fontSize:this.props.width}}>Saucer</p>
+        )
+    }
+} 
